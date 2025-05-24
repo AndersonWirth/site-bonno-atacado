d6c4ca5d4654788ad92599841f069d752bef7031
Anderson Wirth <andersonwirth1@gmail.com>
2025-05-24 12:26:28 -0300

    Ajuste margin.

--- app/components/pages/noticias/noticias-list/index.tsx
+++ app/components/pages/noticias/noticias-list/index.tsx
@@ -12,7 +12,7 @@ type ProjectsListProps = {
 
 export const ProjectsList = ({ noticias }: ProjectsListProps) => {
   return (
-    <section className="container py-32 grid grid-cols-1 sm:grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-x-4 gap-y-6">
+    <section className="container grid grid-cols-1 sm:grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-x-4 gap-y-6">
       {noticias.map((noticia, i) => (
         <motion.div
           key={i}
